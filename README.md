07eb2631ea998a4bb41d0939d4b2b93e4c36d295
soniabotsarova <sonia.botsarova@gmail.com>
2021-10-29 22:05:31 +0100

    add multiply operation

--- README.md
+++
@@ -1,3 +0,0 @@
-# web-platform-qurjrj
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qurjrj)
--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 const myOutput = document.getElementById('output');
 const myForm = document.querySelector('form');
-
-// Defining some arrays
-
-let numbersOne = [1, 4, 5, 7];
-let numbersTwo = [23, 4, 65, 12];
-let numbersThree = [1, 2];
-
-// Use the filter method to fill an array with the numbers which are divisible by two
-
-const evenNum = numbersOne.filter((num) => num % 2 === 0);
-
-//Use the reduce method, to return the total of each array
-
-const arrSum = numbersOne.reduce(
-  (previousNum, currentNum) => previousNum + currentNum
-);
-
 const mySum = document.getElementById('sum');
 const myEven = document.getElementById('even');
+const myMulti = document.getElementById('multiply');
 const myText = document.getElementById('myText');
 
 myForm.addEventListener('submit', (event) => {
@@ -29,21 +13,18 @@ myForm.addEventListener('submit', (event) => {
     (previousNum, currentNum) => previousNum + currentNum
   );
   const even = numbers.filter((num) => num % 2 === 0);
+
+  const multiply = numbers.map((num) => num * 2);
+
   if (numbers.length < 2) {
-    myOutput.innerHTML = `Enter at least 2 numbers, separated by commas`;
+    myOutput.innerHTML = `Enter at least 2 numbers, separated by commas.`;
   } else if (myEven.checked) {
     myOutput.innerHTML = `The even numbers are: ${even}.`;
   } else if (mySum.checked) {
     myOutput.innerHTML = `The sum of the numbers is ${sum}.`;
+  } else if (myMulti.checked) {
+    myOutput.innerHTML = `Your numbers now are ${multiply}.`;
   } else {
     myOutput.innerHTML = `Select an operation`;
   }
 });
-
-// 4️⃣ Add JavaScript so that when the user clicks the button, one of the functions is run and the result is shown on the page.
-
-// The input should be given in the first input box as a string. The user should be able to enter numbers, separated by commas. In JavaScript, you'll convert this string to an array of numbers.
-
-// Based on which radio button is selected, choose which function to run. Pass an array as an argument to the particular function.
-
-// Finally, return the result from the function and display it on the page.
